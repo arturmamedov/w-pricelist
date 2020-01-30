5c134413b3603591e9c778ba143a151d6df5ba7b
Artur Mamedov <arturmamedov1993@gmail.com>
2020-01-30 12:19:56 +0100

    A todo for config file added

--- js/withPricelist/config.php
+++ js/withPricelist/config.php
@@ -1,4 +1,5 @@
 <?php
+// @todo: Conf - Deleted main config file and created config.example.php that must be copied for work!
 
 // set the access_token with thw your own, provided by InsuperAdmin CMS
 $config = [
